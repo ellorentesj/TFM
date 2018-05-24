8d3ba4459fa314bc27a32ae1d0b474d37a4de9e5
ellorentesj <ellorentesj@gmail.com>
2018-05-25 00:37:21 +0200

    Procesamiento de los ficheros

--- PreprocesamientoFicheros.R
+++ PreprocesamientoFicheros.R
@@ -2,113 +2,125 @@
 
 #### 1. PREPROCESAMIENTO DE LOS FICHEROS: Lectura de ficheros y primera limpieza de variables innecesarias ####
 
-list.of.packages <- c("data.table")
+# Los datos de los vuelos se obtienen de: https://www.transtats.bts.gov/DL_SelectFields.asp
+
+list.of.packages <- c("data.table","utils","dplyr")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
 
 library(data.table)
+library(utils)
+library(dplyr)
 
-# Selección de ruta donde están los ficheros, en mi caso: "/Users/ellorentesj/Desktop/TFM/data"
-setwd("/Users/ellorentesj/Desktop/TFM/data/")
+# Selección de ruta donde están los ficheros, en mi caso: "/Users/ellorentesj/repostGitHub/TFM"
+setwd("/Users/ellorentesj/repostGitHub/TFM/data/")
 
-limpiarFichero <- function (mesFichero){
+# Declaro variables 'tempDec' y 'tempJan' como archivos temporeales
+tempDec <- tempfile()
+tempJan <- tempfile()
+
+# Asigno las variables a los archivos
+tempDec = "On_Time_On_Time_Performance_2013_12.csv.zip"
+tempJan = "On_Time_On_Time_Performance_2014_1.csv.zip"
+
+# Utilizo la función unzip para extraer los archivos CSV
+unzip(tempDec,"On_Time_On_Time_Performance_2013_12.csv")
+unzip(tempJan, "On_Time_On_Time_Performance_2014_1.csv")
+
+# Introduzco los datos de los CSV en dataframes
+dfDecember <- fread("On_Time_On_Time_Performance_2013_12.csv", header=T, sep=',')
+dfJanuary <- fread("On_Time_On_Time_Performance_2014_1.csv", header=T, sep=',')
+
+# Elimino la referencia a los archivos temporales
+rm(tempDec)
+rm(tempJan)
+
+flightsAux <- rbind(dfDecember,dfJanuary)
+
+# Limpieza del dataset, elimino las variables que tienen >99.99 perdida de información, puesto que
+# esto indica que es una variable sin datos válidos. Y elimino los datos no significativos que no 
+# aportan ningún tipo de información para el análisis.
+clenEmptyData <- function (dfFlightsAux){
   
-  # Lectura del fichero
-  vueDF <- fread(mesFichero, header=T, sep=',')
+  dfFlightsAux$Quarter <- NULL
+  dfFlightsAux$TailNum <- NULL
+  dfFlightsAux$OriginStateFips <- NULL
+  dfFlightsAux$OriginStateName <- NULL
+  dfFlightsAux$OriginWac <- NULL
+  dfFlightsAux$DestStateFips <- NULL
+  dfFlightsAux$DestStateName <- NULL
+  dfFlightsAux$DestWac <- NULL
+  dfFlightsAux$CRSDepTime <- NULL
+  dfFlightsAux$DepartureDelayGroups <- NULL
+  dfFlightsAux$DepTimeBlk <- NULL
+  dfFlightsAux$WheelsOff <- NULL
+  dfFlightsAux$WheelsOn <- NULL
+  dfFlightsAux$CRSArrTime <- NULL
+  dfFlightsAux$ArrivalDelayGroups <- NULL
+  dfFlightsAux$ArrTimeBlk <- NULL
+  dfFlightsAux$Diverted <- NULL
+  dfFlightsAux$CRSElapsedTime <- NULL
+  dfFlightsAux$ActualElapsedTime <- NULL
+  dfFlightsAux$Flights <- NULL
+  dfFlightsAux$DistanceGroup <- NULL
+  dfFlightsAux$FirstDepTime <- NULL
+  dfFlightsAux$TotalAddGTime <- NULL
+  dfFlightsAux$LongestAddGTime <- NULL
+  dfFlightsAux$DivAirportLandings <- NULL
+  dfFlightsAux$DivReachedDest <- NULL
+  dfFlightsAux$DivActualElapsedTime <- NULL
+  dfFlightsAux$DivArrDelay <- NULL
+  dfFlightsAux$DivDistance <- NULL
+  dfFlightsAux$Div1Airport <- NULL
+  dfFlightsAux$Div1AirportID <- NULL
+  dfFlightsAux$Div1AirportSeqID <- NULL
+  dfFlightsAux$Div1WheelsOn <- NULL
+  dfFlightsAux$Div1TotalGTime <- NULL
+  dfFlightsAux$Div1LongestGTime <- NULL
+  dfFlightsAux$Div1WheelsOff <- NULL
+  dfFlightsAux$Div1TailNum <- NULL
+  dfFlightsAux$Div2Airport <- NULL
+  dfFlightsAux$Div2AirportID <- NULL
+  dfFlightsAux$Div2AirportSeqID <- NULL
+  dfFlightsAux$Div2WheelsOn <- NULL
+  dfFlightsAux$Div2TotalGTime <- NULL
+  dfFlightsAux$Div2LongestGTime <- NULL
+  dfFlightsAux$Div2WheelsOff <- NULL
+  dfFlightsAux$Div2TailNum <- NULL
+  dfFlightsAux$Div3Airport <- NULL
+  dfFlightsAux$Div3AirportID <- NULL
+  dfFlightsAux$Div3AirportSeqID <- NULL
+  dfFlightsAux$Div3WheelsOn <- NULL
+  dfFlightsAux$Div3TotalGTime <- NULL
+  dfFlightsAux$Div3LongestGTime <- NULL
+  dfFlightsAux$Div3WheelsOff <- NULL
+  dfFlightsAux$Div3TailNum <- NULL
+  dfFlightsAux$Div4Airport <- NULL
+  dfFlightsAux$Div4AirportID <- NULL
+  dfFlightsAux$Div4AirportSeqID <- NULL
+  dfFlightsAux$Div4WheelsOn <- NULL
+  dfFlightsAux$Div4TotalGTime <- NULL
+  dfFlightsAux$Div4WheelsOff <- NULL
+  dfFlightsAux$Div4LongestGTime <- NULL
+  dfFlightsAux$Div4TailNum <- NULL
+  dfFlightsAux$Div5Airport <- NULL
+  dfFlightsAux$Div5AirportID <- NULL
+  dfFlightsAux$Div5AirportSeqID <- NULL
+  dfFlightsAux$Div5WheelsOn <- NULL
+  dfFlightsAux$Div5TotalGTime <- NULL
+  dfFlightsAux$Div5LongestGTime <- NULL
+  dfFlightsAux$Div5WheelsOff <- NULL
+  dfFlightsAux$Div5TailNum <- NULL
+  dfFlightsAux$V110 <- NULL
   
-  # Limpieza de los datos no significativos. Se eliminan los datos que no aportan ningún tipo de de información para el análisis de predicción. 
-  vueDF$Quarter <- NULL 
-  vueDF$TailNum <- NULL
-  vueDF$OriginStateFips <- NULL
-  vueDF$OriginStateName <- NULL
-  vueDF$OriginWac <- NULL
-  vueDF$DestStateFips <- NULL
-  vueDF$DestStateName <- NULL
-  vueDF$DestWac <- NULL
-  vueDF$CRSDepTime <- NULL
-  vueDF$DepartureDelayGroups <- NULL
-  vueDF$DepTimeBlk <- NULL
-  vueDF$WheelsOff <- NULL
-  vueDF$WheelsOn <- NULL
-  vueDF$CRSArrTime <- NULL
-  vueDF$ArrivalDelayGroups <- NULL
-  vueDF$ArrTimeBlk <- NULL
-  vueDF$Diverted <- NULL
-  vueDF$CRSElapsedTime <- NULL
-  vueDF$ActualElapsedTime <- NULL
-  vueDF$AirTime <- NULL
-  vueDF$Flights <- NULL
-  vueDF$DistanceGroup <- NULL
-  vueDF$FirstDepTime <- NULL
-  vueDF$TotalAddGTime <- NULL
-  vueDF$LongestAddGTime <- NULL
-  vueDF$DivAirportLandings <- NULL
-  vueDF$DivReachedDest <- NULL
-  vueDF$DivActualElapsedTime <- NULL
-  vueDF$DivArrDelay <- NULL
-  vueDF$DivDistance <- NULL
-  vueDF$Div1Airport <- NULL
-  vueDF$Div1AirportID <- NULL
-  vueDF$Div1AirportSeqID <- NULL
-  vueDF$Div1WheelsOn <- NULL
-  vueDF$Div1TotalGTime <- NULL
-  vueDF$Div1LongestGTime <- NULL
-  vueDF$Div1WheelsOff <- NULL
-  vueDF$Div1TailNum <- NULL
-  vueDF$Div2Airport <- NULL
-  vueDF$Div2AirportID <- NULL
-  vueDF$Div2AirportSeqID <- NULL
-  vueDF$Div2WheelsOn <- NULL
-  vueDF$Div2TotalGTime <- NULL
-  vueDF$Div2LongestGTime <- NULL
-  vueDF$Div2WheelsOff <- NULL
-  vueDF$Div2TailNum <- NULL
-  vueDF$Div3Airport <- NULL
-  vueDF$Div3AirportID <- NULL
-  vueDF$Div3AirportSeqID <- NULL
-  vueDF$Div3WheelsOn <- NULL
-  vueDF$Div3TotalGTime <- NULL
-  vueDF$Div3LongestGTime <- NULL
-  vueDF$Div3WheelsOff <- NULL
-  vueDF$Div3TailNum <- NULL
-  vueDF$Div4Airport <- NULL
-  vueDF$Div4AirportID <- NULL
-  vueDF$Div4AirportSeqID <- NULL
-  vueDF$Div4WheelsOn <- NULL
-  vueDF$Div4TotalGTime <- NULL
-  vueDF$Div4WheelsOff <- NULL
-  vueDF$Div4LongestGTime <- NULL
-  vueDF$Div4TailNum <- NULL
-  vueDF$Div5Airport <- NULL
-  vueDF$Div5AirportID <- NULL
-  vueDF$Div5AirportSeqID <- NULL
-  vueDF$Div5WheelsOn <- NULL
-  vueDF$Div5TotalGTime <- NULL
-  vueDF$Div5LongestGTime <- NULL
-  vueDF$Div5WheelsOff <- NULL
-  vueDF$Div5TailNum <- NULL
-  vueDF$V110 <- NULL
-  
-  return (vueDF)
+  return (dfFlightsAux)
 }
 
-dic = "On_Time_On_Time_Performance_2013_12.csv"
-ene = "On_Time_On_Time_Performance_2014_1.csv"
+flights <- clenEmptyData(flightsAux)
 
-# ficCSV <- list.files('data/2013/', pattern = '*.csv', full.names = T)
-# vuelos <- lapply(ficCSV, fread)
+write.table(flights, file = "flights.csv", append = FALSE, sep = ",", eol = "\n", row.names = FALSE, na = "", col.names = TRUE)
 
-# Limpieza del mes de Diciembre para escribirlo en el csv
-dicdf <- limpiarFichero(dic)
-write.table(dicdf, file = "vuelos.csv", append = FALSE, sep = ",", eol = "\n", row.names = FALSE, na = "", col.names = TRUE)
-enedf <- limpiarFichero(ene)
-write.table(enedf, file = "vuelos.csv", append = TRUE, sep = ",", eol = "\n", row.names = FALSE, na = "", col.names = FALSE)
-
-# lismes <- c(ene,feb)
-# 
-# for (i in lismes){
-#   
-#   mesdf <- limpiarFichero(i)
-#   write.table(mesdf, file = "vuelos.csv", append = TRUE, sep = ",", eol = "\n", row.names = FALSE, na = "", col.names = FALSE)
-#   
-# }
+# Elimino los datos no necesarios
+rm(dfDecember)
+rm(dfJanuary)
+rm(flightsAux)
